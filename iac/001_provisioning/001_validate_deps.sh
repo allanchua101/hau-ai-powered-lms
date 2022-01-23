2e9dc3eca4f146e703689572f9b5de1c4af81b69
Allan Chua <allanchua.officefiles@gmail.com>
2022-01-23 11:20:19 +0800

    Create 001_validate_deps.sh

---
+++ iac/001_provisioning/001_validate_deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script is designed for checking all the
+# dependencies required by terraform to function properly.
+set -e
+
+# Check if terraform is installed
+# Check if aws cli is installed
+# Check if named profile is existing
+# Check if state s3 is existing
+# Check if lock dynamodb is existing
